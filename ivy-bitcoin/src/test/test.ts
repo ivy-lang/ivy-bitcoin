7e68d6c77baf9a28ff8ead7d987654eedc5014b9
Dan Robinson <danrobinson010@gmail.com>
2018-01-25 11:13:16 -0800

    Revert "compiler: add optimization to rearrange statements that include checksequenceverify and checklocktimeverify"
    This was probably a premature optimization.

--- ivy-bitcoin/src/test/test.ts
+++ ivy-bitcoin/src/test/test.ts
@@ -25,7 +25,6 @@ describe("compile", () => {
       const contractSource = TEST_CASES[name]
       const compiled = compile(contractSource)
       expect((compiled as CompilerError).message).to.equal(undefined) // so it prints the error
-      console.log((compiled as Template).instructions)
     })
   })
   Object.keys(ERRORS).forEach(errorName => {
