e5bd0c49de89735f0044394453abb53561e4e401
Dan Robinson <danrobinson010@gmail.com>
2018-04-06 23:55:31 -0700

    change crypto dependencies to use bcrypto

--- ivy-bitcoin/src/crypto.ts
+++ ivy-bitcoin/src/crypto.ts
@@ -1,9 +1,10 @@
-import { crypto, hd, keyring } from "bcoin"
+import { hd, keyring } from "bcoin"
+import * as crypto from "bcrypto"
 
 export const sha1 = crypto.sha1
 export const sha256 = crypto.sha256
 export const ripemd160 = crypto.ripemd160
-export const sign = crypto.secp256k1.sign
+export const sign = crypto.secp256k1.signDER
 export const fromDER = crypto.secp256k1.fromDER
 export const randomBytes = crypto.randomBytes
 export const privateKey = hd.PrivateKey
