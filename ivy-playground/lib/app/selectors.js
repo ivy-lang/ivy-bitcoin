99881c2107507dfc2ad7d9dd1402d38ce82193f4
Dan Robinson <danrobinson010@gmail.com>
2018-04-07 11:18:09 -0700

    about to rebase

--- ivy-playground/lib/app/selectors.js
+++ ivy-playground/lib/app/selectors.js
@@ -1,6 +1,6 @@
 export const getAppState = (state) => {
     if (state.getIn !== undefined) {
-        return state.getIn(["ivy-plugin", "ivyState"]);
+        return state.getIn(["plugins", "ivy-plugin", "ivyState"]);
     }
     else {
         return state;
