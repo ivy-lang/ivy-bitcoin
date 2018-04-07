5140fdb7b0cf621b7aabb1416cf0fc5d024ea243
Dan Robinson <danrobinson010@gmail.com>
2018-04-07 11:13:28 -0700

    change back, lol

--- ivy-playground/src/app/selectors.tsx
+++ ivy-playground/src/app/selectors.tsx
@@ -1,6 +1,5 @@
 import { AppState } from "./types"
 
-
 export const getAppState = (state: AppState | any) => {
   if (state.getIn !== undefined) {
     return state.getIn(["plugins", "ivy-plugin", "ivyState"])
